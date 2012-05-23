36f373cc04d5bac687174c55f12abbd34c2df66a
James Huynh <james@rubify.com>
2012-05-23 10:23:23 +0800

    add an example_filter to application_controller and add skip all filters for rails/routes

--- app/controllers/rails/routes_controller.rb
+++ app/controllers/rails/routes_controller.rb
@@ -1,6 +1,8 @@
 class Rails::RoutesController < ApplicationController
   layout 'rails/routes'
 
+  before_filter :ensure_local
+  
   # Skipp all filters for this this rails/routes :-)
   skip_filter(*_process_action_callbacks.map(&:filter)) 
   
