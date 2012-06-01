e7797c3fcddafddd1e66e410775284c8199301c9
schneems <richard.schneeman@gmail.com>
2012-06-01 10:54:57 +0200

    comment out capybara debugging line

--- test/integration/sextant_test.rb
+++ test/integration/sextant_test.rb
@@ -4,7 +4,7 @@ class InheritNavigationTest < ActiveSupport::IntegrationCase
 
   test 'default index' do
     visit('/rails/routes')
-    save_and_open_page
+    # save_and_open_page
     assert has_content?('foo_index')
     assert has_content?('/foo(.:format)')
     assert has_content?('foo#index')
