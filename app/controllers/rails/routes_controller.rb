bc221f88a1acf01f20ca5f2386ddf5990f47444b
schneems <richard.schneeman@gmail.com>
2012-06-08 11:27:52 -0500

    better local code

--- app/controllers/rails/routes_controller.rb
+++ app/controllers/rails/routes_controller.rb
@@ -1,15 +1,21 @@
 class Rails::RoutesController < ActionController::Base
   layout 'rails/routes'
 
-  before_filter :ensure_local
+  before_filter :require_local!
 
   def index
     @routes = Sextant.format_routes
   end
 
   private
-  def ensure_local
-    return false if ActionView::Resolver.caching
+  def require_local!
+    unless local_request?
+      render :text => '<p>For security purposes, this information is only available to local requests.</p>', :status => :forbidden
+    end
+  end
+
+  def local_request?
+    Rails.application.config.consider_all_requests_local || request.local?
   end
 
 end
