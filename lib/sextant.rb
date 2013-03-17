72f530b53a5a7f4bef07f6af1357402a0222fe65
Ahmad Sherif <ahmad.sherif@mashsolvents.com>
2013-03-17 14:59:20 +0200

    Show mounted engines routes

--- lib/sextant.rb
+++ lib/sextant.rb
@@ -6,7 +6,10 @@ module Sextant
   def self.format_routes(routes = all_routes)
     # ActionDispatch::Routing::RoutesInspector.new.collect_routes(_routes.routes)
     inspector = Rails::Application::RouteInspector.new
-    inspector.collect_routes(routes)
+    main_app_routes = inspector.collect_routes(routes)
+    engines_routes = inspector.instance_eval { @engines } # No direct way to access engines rotues
+
+    {main_app: main_app_routes, engines: engines_routes}
   end
 
   def self.all_routes
