c12e4a46517b470dc8071dcc03274f64467d94de
unalterable <diamond.oliver@gmail.com>
2016-07-20 14:27:26 +0100

    Route links working

--- app/controllers/sextant/diagrams_controller.rb
+++ app/controllers/sextant/diagrams_controller.rb
@@ -5,7 +5,15 @@ module Sextant
 #    before_filter :require_local!
 
     def index
-#      @routes = Sextant.format_routes
+      id = params[:id].to_i || 0
+      
+      route = Sextant.format_routes[id][:reqs].split('#')
+      
+      classname, methodname = route
+      classname = (classname + '_controller').classify
+
+      file, line = eval(classname).instance_method(methodname.to_sym).source_location
+      @routeinfo = "Method '##{methodname}' is defined in #{file}, line #{line}"
     end
 
   end
