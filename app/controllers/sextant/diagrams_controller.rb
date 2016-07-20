cfb9c66ead270d50fe6d36cd715a823d4c58dd2d
unalterable <diamond.oliver@gmail.com>
2016-07-20 10:55:51 +0100

    Added Diagrams route

---
+++ app/controllers/sextant/diagrams_controller.rb
@@ -0,0 +1,13 @@
+module Sextant
+  class DiagramsController < Sextant::ApplicationController
+    layout 'sextant/application'
+
+#    before_filter :require_local!
+
+    def index
+#      @routes = Sextant.format_routes
+    end
+
+  end
+end
+
