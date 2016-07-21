e9274e0f22e2798bf2f7a0acfd45730d5d2d9e73
Apoorva Saxena <apsaxena24@gmail.com>
2016-07-21 10:38:27 +0100

    few changes in controller

--- app/controllers/sextant/diagrams_controller.rb
+++ app/controllers/sextant/diagrams_controller.rb
@@ -1,18 +1,21 @@
 module Sextant
-  require 'method_source'
+
 
   class DiagramsController < Sextant::ApplicationController
     layout 'sextant/application'
 
     def index
+
       id = params[:id].to_i || 0
       route = Sextant.format_routes[id][:reqs].split('#')
       classname, methodname = route
       classname = eval((classname + '_controller').classify)
+      require 'method_source'
       file, line = classname.instance_method(methodname.to_sym).source_location
       @routeinfo = "Method '##{methodname}' is defined in #{file}, line #{line}"
       file = classname.instance_method(methodname.to_sym).source
       @file = file.gsub("\n", "<br>").html_safe
+
     end
   end
 end
